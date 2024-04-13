edcc180984a809b455c1a0a5887ed814169f80a2
DanielY <danielyariv21@gmail.com>
2024-04-13 22:09:09 +0300

    adding search, finish routing

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,4 +1,10 @@
-import { Component, Input, OnInit } from '@angular/core';
+import {
+  Component,
+  Input,
+  OnInit,
+  ChangeDetectorRef,
+  OnDestroy,
+} from '@angular/core';
 
 import { ProductsService } from './../products.service';
 
@@ -9,12 +15,12 @@ import { ButtonModule } from 'primeng/button';
 import { DataViewModule } from 'primeng/dataview';
 import { RatingModule } from 'primeng/rating';
 import { TagModule } from 'primeng/tag';
-import { PaginatorModule } from 'primeng/paginator';
 
 import { NgbAlert } from '@ng-bootstrap/ng-bootstrap';
 
 import { ProductComponent } from '../product/product.component';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-products',
@@ -28,51 +34,63 @@ import { ActivatedRoute } from '@angular/router';
     RatingModule,
     FormsModule,
     TagModule,
-    PaginatorModule,
   ],
   templateUrl: './products.component.html',
   styleUrl: './products.component.css',
 })
-export class ProductsComponent implements OnInit {
+export class ProductsComponent implements OnInit, OnDestroy {
   @Input() products: any[] = [];
   category: string | null = null;
   subCategory: string | null = null;
+  filteredProducts: any[] = [];
+  routeSubscription: Subscription = new Subscription();
+
   constructor(
     private productsService: ProductsService,
-    private activatedRoute: ActivatedRoute
+    private activatedRoute: ActivatedRoute,
+    private router: Router,
+    private cdr: ChangeDetectorRef // for detect the changes between different subCategories like electronics/apple electronics/android
   ) {}
 
   ngOnInit(): void {
-    this.findCategory();
-    this.findSubCategory();
+    this.routeSubscription = this.activatedRoute.paramMap.subscribe(
+      (params) => {
+        if (this.router.url != '/home') {
+          //if the user choose home, skip the code below because the products need to include only the best sellers products, get it from the property bind in the home component
+          this.category = params.get('category');
+          this.subCategory = params.get('subCategory');
+          this.updateProductList();
+          this.cdr.detectChanges(); // trigger check detection manually
+        }
+        this.filteredProducts = this.products;
+      }
+    );
+  }
 
-    if (this.category === 'Electronics') {
-      this.products = this.productsService.getByCategory(this.category);
-    }
-
-    if (this.subCategory != null) {
+  //render to screen the product list based on the subCategory, if null based on Category, if null show all products
+  updateProductList() {
+    if (this.subCategory) {
       this.products = this.productsService.getByCategoryAndSubCategory(
         this.category,
         this.subCategory
       );
-      console.log(this.products);
-    }
-
-    if (this.products.length == 0) {
+    } else if (this.category) {
+      this.products = this.productsService.getByCategory(this.category);
+    } else {
       this.products = this.productsService.getProducts();
     }
   }
 
-  findCategory() {
-    this.activatedRoute.paramMap.subscribe((params) => {
-      this.category = params.get('category');
-      console.log(this.category);
-    });
+  onSearch(event: any) {
+    let query = event.target.value.toLowerCase();
+    this.filteredProducts = this.products.filter((product) =>
+      product.Name.toLowerCase().includes(query)
+    );
   }
-  findSubCategory() {
-    this.activatedRoute.paramMap.subscribe((params) => {
-      this.subCategory = params.get('subCategory');
-      console.log(this.subCategory);
-    });
+
+  ngOnDestroy(): void {
+    if (this.routeSubscription) {
+      this.routeSubscription.unsubscribe();
+    }
   }
 }
